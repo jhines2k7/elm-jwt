92878f5862137032a46a20a7bc74743fafa8e6af
James Hines <jameshines10@gmail.com>
2016-09-01 21:44:41 -0400

    Adds init and update functions

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,52 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.App as Html
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
+
+
+{-
+   MODEL
+   * Model type
+   * Initialize model with empty values
+-}
+
+
+type alias Model =
+    { quote : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "", Cmd.none )
+
+
+
+{-
+   UPDATE
+   * Messages
+   * Update case
+-}
+
+
+type Msg
+    = GetQuote
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GetQuote ->
+            ( { model | quote = model.quote ++ "A quote! " }, Cmd.none )
+
+
+main : Program Never
+main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
